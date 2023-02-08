45e4178c7c4c442e08db97fe1a1bc6eba5f2914e
Daniel Seppo Eke <juniorseppo3@gmail.com>
2023-02-08 13:49:30 +0100

    08 fevrier

--- src/Layouts/screen.jsx
+++ src/Layouts/screen.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import './screen.css'
 
 export default function Screen({children}){
-    return <div className="screen">
+    return (<div className="screen">
         <div className="containerFull">
             {children}
         </div>
-    </div>
+    </div>)
 }
 export function Normale_Screen({children}){
     return (
